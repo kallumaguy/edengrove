c74fccd199df281b9924a31f7df095a12d2ffd7d
kallumaguy <adilahmed.aa786@gmail.com>
2025-01-23 17:47:19 +0530

    Updated directories

---
+++ Frontend/src/routers/router.jsx
@@ -0,0 +1,21 @@
+import {
+    createBrowserRouter,
+    
+} from "react-router-dom";
+import App from "../App";
+import Home from "../pages/home/home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            }
+        ],
+    },
+]);
+
+export default router;
